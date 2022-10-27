30a2b612f5757df4993165824ba3b7678c0382f4
Andrew M. Adams <65849631+WIZBRAT34@users.noreply.github.com>
2022-10-27 14:21:25 -0400

    #Added addresses

--- main.c
+++ main.c
@@ -6,6 +6,28 @@ Discription : Just testing code for STM32F4 board:
           
 */
 
+/*
+BASE ADDRESS
+*/
+#define AHB1_BASE     0x40020000
+#define GPIOA_BASE   (AHB1_BASE)
+#define GPIOB_BASE   (AHB1_BASE  +400)
+#define GPIOC_BASE   (GPIOB_BASE +400)
+#define GPIOD_BASE   (GPIOC_BASE +400)
+#define GPIOE_BASE   (GPIOD_BASE +400)
+#define RCC_BASE     (AHB1_BASE  + 0x00003800)
+
+
+/*
+SUB ADDRESSES
+*/
+#define RCC_AHB1_EN   (RCC_BASE+0x30)
+#define GPIOA_MOD     (GPIOA_BASE +0x00)
+#define GPIOA_ODR     (GPIOA_BASE +0x14)
+
+
+
+
 
 #include <stdio.h>
 
