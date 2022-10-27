679a8d84f05774b321f9a8917ddeac8d4fcc106a
Andrew M. Adams <65849631+WIZBRAT34@users.noreply.github.com>
2022-10-27 15:42:10 -0400

    #Registers created

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ Discription : Just testing code for STM32F4 board:
 /*
 BASE ADDRESS
 */
-#define AHB1_BASE     0x40020000
+#define AHB1_BASE     0x40020000UL
 #define GPIOA_BASE   (AHB1_BASE)
 #define GPIOB_BASE   (AHB1_BASE  +400)
 #define GPIOC_BASE   (GPIOB_BASE +400)
@@ -21,9 +21,9 @@ BASE ADDRESS
 /*
 SUB ADDRESSES
 */
-#define RCC_AHB1_EN   (RCC_BASE+0x30)
-#define GPIOA_MOD     (GPIOA_BASE +0x00)
-#define GPIOA_ODR     (GPIOA_BASE +0x14)
+#define RCC_AHB1      (*(volatile unsigned int *)(RCC_BASE+0x30))
+#define GPIOA_MOD     (*(volatile unsigned int *)(GPIOA_BASE +0x00))
+#define GPIOA_ODR     (*(volatile unsigned int *)(GPIOA_BASE +0x14))
 
 
 
@@ -33,10 +33,12 @@ SUB ADDRESSES
 
 
 int main(){
-  
-  whille(1){
-    
+   printf("end of program");
+   
+  while(1){
+      
   }
  
   return 0;
 }
+
