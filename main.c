4c215128976fe2d0b0ac184944017bce813c00a9
Andrew M. Adams <65849631+WIZBRAT34@users.noreply.github.com>
2022-10-27 13:12:11 -0400

    started writing main code for blinking PA5 led

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+Author      : Andrew Adams
+date        : null
+Discription : Just testing code for STM32F4 board: 
+              *blink PA5
+          
+*/
+
+
+#include <stdio.h>
+
+
+int main(){
+  
+  whille(1){
+    
+  }
+ 
+  return 0;
+}
